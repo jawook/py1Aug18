4acd8a80acd485d95a53c5e719c60dfcf97b6806
jawook <95268438+jawook@users.noreply.github.com>
2023-08-18 10:31:47 -0400

    firstUP

---
+++ MorningNotes.py
@@ -0,0 +1,61 @@
+
+# COMMENT HEAVILY
+    # the # is the signifier for a comment
+    print("Hello World") # this will print hello world
+    # Create a comment line # at the beginning: CTRL + 1
+    # This is a comment
+    # Comments are a great way to create pseudo code
+    # Comments do not execute in python
+    
+#%% Section Break - Let's you organize your code
+
+# Code
+
+#%%% Sub-Section
+
+# Subsection
+
+#%% Keyboard shortcuts in Spyder
+    # 1) Ribbon
+    # 2) Shortcuts
+        # F9 -> runs a single line of code
+        # CTRL + Enter -> run a 'cell' of code
+        # F5 -> runs all of your code
+        # CTRL + 1 to comment
+    # See all: Tools -> Preferences -> Keyboard Shortcuts
+    
+#%% Data types in python
+
+# Main types of information you will deal with:
+    # 1) Numerical
+    # 2) Strings -> text
+    
+# Types of numberical values
+    # 1) Numbers
+        # 1) Integers
+        # 2) Float
+    # 2) Dates
+    
+    # to create a variable varName = ...
+x = 5
+type(x)
+y = 2.2
+type(y)
+
+# We can change a number's type by 'casting'
+z = 5
+z = float(z)
+
+x + y
+
+# When casting to int you discard all precision
+w = 5.7
+w = int(w)
+
+# Page 15 shows all math operators
+# Incrementers
+x = x + 1
+x += 1
+
+#%% Strings in python
+
