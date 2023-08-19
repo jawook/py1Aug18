79b1502be1a431e73de7b4ec193fdbecb18bb506
jawook <95268438+jawook@users.noreply.github.com>
2023-08-19 14:41:11 -0400

    finalUpdate

---
+++ otherHelpfulResources.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 10:38:54 2023
+
+@author: wilkijam
+"""
+
+# Helpful resources to look at as you apply python.
+
+# Resources from Bogdan (one of the Co-Heads of our Data Science team)
+    # https://github.com/dbogt
+
+# Numpy documentation: https://numpy.org/devdocs/
+# Numpy cheat sheet: https://web.itu.edu.tr/iguzel/files/Python_Cheat_Sheets.pdf
+
+# Pandas documentation: https://pandas.pydata.org/docs/
+# Pandas cheat sheet: https://pandas.pydata.org/Pandas_Cheat_Sheet.pdf
+
+# Streamlit information & examples (used to create the dashboards I demo'd):
+    # https://streamlit.io/
+# Example dashboards shown:
+    # Taxes: https://diamond-valley-taxes.streamlit.app/
+    # AB Power Production: https://energyminuteabpowergeneration.streamlit.app/
+    # Gas Station Mapping: https://dbogt-locationsdemo-app-avfon4.streamlit.app/
